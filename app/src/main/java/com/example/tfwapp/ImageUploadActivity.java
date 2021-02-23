a57227d22d84ac488a08924ce994c5a43852cdc4
Deric Wright <dericwright@usf.edu>
2021-02-23 14:54:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tfwapp/ImageUploadActivity.java
@@ -0,0 +1,17 @@
+package com.example.tfwapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_upload);
+        
+    }
+
+
+}
